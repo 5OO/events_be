94ecd4aef446b238aa1ce36d70974eff8da170fe
5OO <git.hub@myrk.eu>
2024-05-01 12:09:15 +0300

    Add database schema and initial data
    Created tables for Events, Individuals, and Companies in a new schema_file-db.sql and added initial data in a new data_file-db.sql. Updated application.properties to link to the new database files and set Hibernate ddl-auto property to update rather than create.

---
+++ src/main/resources/db/schema_file-db.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Events (
+                        EventID INT AUTO_INCREMENT PRIMARY KEY,
+                        EventName VARCHAR(255),
+                        EventDateTime DATETIME,
+                        Location VARCHAR(255),
+                        AdditionalInfo VARCHAR(1000)
+);
+
+CREATE TABLE Individuals (
+                             ParticipantID INT AUTO_INCREMENT PRIMARY KEY,
+                             EventID INT,
+                             FirstName VARCHAR(255),
+                             LastName VARCHAR(255),
+                             PersonalID VARCHAR(255),
+                             PaymentMethod VARCHAR(255),
+                             AdditionalInfo VARCHAR(1500),
+                             FOREIGN KEY (EventID) REFERENCES Events(EventID)
+);
+
+CREATE TABLE Companies (
+                           ParticipantID INT AUTO_INCREMENT PRIMARY KEY,
+                           EventID INT,
+                           LegalName VARCHAR(255),
+                           RegistrationCode VARCHAR(255),
+                           NumberOfParticipants INT,
+                           PaymentMethod VARCHAR(255),
+                           AdditionalInfo VARCHAR(5000),
+                           FOREIGN KEY (EventID) REFERENCES Events(EventID)
+);
