c42a55af6a7259239387a537324c732d3fcdfa00
5OO <git.hub@myrk.eu>
2024-05-01 22:27:51 +0300

    Add global exception handler
    A new file, GlobalExceptionHandler.java, was created to manage IllegalArgumentExceptions globally. It ensures that when this exception is thrown, a bad request HTTP status is returned with the exception message.

---
+++ src/main/java/org/regikeskus/events/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package org.regikeskus.events.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
