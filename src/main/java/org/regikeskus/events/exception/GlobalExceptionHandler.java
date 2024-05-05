f7dd26b56d42959bb8af75a38ad5acf8a312c82b
5OO <git.hub@myrk.eu>
2024-05-05 08:29:18 +0300

    Remove NoSuchElementException from GlobalExceptionHandler
    The unused NoSuchElementException handler has been removed from the GlobalExceptionHandler class. This handler was previously used to handle situations where a requested resource could not be found.

--- src/main/java/org/regikeskus/events/exception/GlobalExceptionHandler.java
+++ src/main/java/org/regikeskus/events/exception/GlobalExceptionHandler.java
@@ -5,8 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.NoSuchElementException;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -23,10 +21,5 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException ex) {
-        return new ResponseEntity<>("Resource not found", HttpStatus.NOT_FOUND);
     }
 }
